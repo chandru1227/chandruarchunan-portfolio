47c0e2325502b01cb6e2e25850bc01f8cea5a23c
Chandru Archunan <168585041+chandru1227@users.noreply.github.com>
2025-04-30 23:54:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Social from './components/Social';
 import About from './components/About';
 import Project from './components/Project';
 import Experience from './components/Experience';
-import Certificates from './components/Certificates'; // Import new component
+import Certificates from './components/Certificates';
 import Contact from './components/Contact';
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <title>Chandru</title>
+        <meta name="description" content="Chandru's portfolio website" />
+      </Helmet>
+      
       <Navbar />
       <Home />
       <About />
       <Project />
       <Experience />
-      <Certificates /> {/* Add new component */}
+      <Certificates />
       <Contact />
       
       <Social />
